eea631bc6af479ca6f28487a00ba61e4fc4529bf
Taowathar <taowathar@gmail.com>
2021-08-26 19:38:22 +0200

    Add Type component

---
+++ src/components/Type.js
@@ -0,0 +1,9 @@
+const Type = ({type}) => {
+    return (
+        <div className="type-card">
+            <h3 className="pokemon-name">{type.name}</h3>
+      </div>
+    )
+}
+
+export default Type
