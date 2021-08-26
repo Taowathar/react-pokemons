3a20ca006a8b0de601bd5f0a7bb4be54b799af3a
Taowathar <taowathar@gmail.com>
2021-08-26 16:18:19 +0200

    Implement fetchPokemons function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import PokemonList from "./components/PokemonList";
 
-function App() {
+const App = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      const pokemonsFromServer = await fetchPokemons();
+      setPokemonList(pokemonsFromServer);
+    };
+    getPokemons();
+  }, []);
+
+  const fetchPokemons = async () => {
+    const results = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
+    );
+    const data = await results.json();
+
+    return data.results;
+  };
+
   return (
     <Router>
       <div className="App">
         <Navbar />
         <div className="container">
-          <Route path="/" exact render={() => 
+          <Route path="/" exact render={() => <></>} />
+          <Route
+            path="/pokemons"
+            exact
+            render={(props) => (
               <>
+                <PokemonList pokemons={pokemonList} />
               </>
-            } 
+            )}
           />
-          <Route path="/pokemons" exact render={(props) => 
-              <>
-              </>
-            } 
-          />
-          <Route path="/types" exact render={(props) => 
-              <>
-             </>
-            } 
-          />
+          <Route path="/types" exact render={(props) => <></>} />
           {/* <Route path="/about" component={About} /> */}
         </div>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
