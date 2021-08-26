7b25184b656b9f09df23013f3d2c74001b0d9476
Taowathar <taowathar@gmail.com>
2021-08-26 18:38:09 +0200

    Add pictures to pokemon cards

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,7 +1,28 @@
-const Pokemon = ({ pokemon }) => {
+import { useState, useEffect } from "react";
+
+const Pokemon = ({ pokemon}) => {
+
+  const [picture, setPicture] = useState()
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      const pictureFromServer = await fetchPokemonPicture();
+      setPicture(pictureFromServer);
+    };
+    getPokemons();
+  }, []);
+
+  const fetchPokemonPicture = async () => {
+    const results = await fetch(pokemon.url);
+    const data = await results.json();
+
+    return data.sprites.front_default;
+  };
+
   return (
     <div className="pokemon-card">
       <h3 className="pokemon-name">{pokemon.name}</h3>
+      <img src={picture} alt="logo" />
     </div>
   );
 };
