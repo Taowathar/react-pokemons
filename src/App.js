8f65fd164c5ee0495e8c2708d1c96ec77babbc64
Taowathar <taowathar@gmail.com>
2021-08-26 18:15:05 +0200

    Implement pagination

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import ReactPaginate from "react-paginate";
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -5,22 +6,35 @@ import PokemonList from "./components/PokemonList";
 
 const App = () => {
   const [pokemonList, setPokemonList] = useState([]);
+  
+  var [currentPage, setCurrentPage] = useState(0);
+  const pageCount = 56;
+  const pokemonsPerPage = 20;
 
   useEffect(() => {
     const getPokemons = async () => {
-      const pokemonsFromServer = await fetchPokemons();
+      const pokemonsFromServer = await fetchPokemons({ currentPage });
       setPokemonList(pokemonsFromServer);
     };
     getPokemons();
   }, []);
 
-  const fetchPokemons = async () => {
-    const results = await fetch(
-      "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
-    );
+  const fetchPokemons = async ({ currentPage }) => {
+    const pokemonsFrom = currentPage * pokemonsPerPage;
+    const pokemonListUrl = `https://pokeapi.co/api/v2/pokemon?offset=${pokemonsFrom}&limit=${pokemonsPerPage}`;
+    const results = await fetch(pokemonListUrl);
     const data = await results.json();
 
     return data.results;
+  };
+
+  const handlePageChange = (selectedObject) => {
+    setCurrentPage(selectedObject.selected);
+    const getPokemons = async () => {
+      const pokemonsFromServer = await fetchPokemons({ currentPage });
+      setPokemonList(pokemonsFromServer);
+    };
+    getPokemons();
   };
 
   return (
@@ -33,8 +47,25 @@ const App = () => {
             path="/pokemons"
             exact
             render={(props) => (
-              <div className="card-container">
-                <PokemonList pokemons={pokemonList} />
+              <div className="x">
+                <div className="card-container">
+                  <PokemonList pokemons={pokemonList} />
+                </div>
+                <div className="pagination-field">
+                  <ReactPaginate
+                    pageCount={pageCount}
+                    pageRange={2}
+                    marginPagesDisplayed={2}
+                    onPageChange={handlePageChange}
+                    containerClassName={"pagination-field"}
+                    previousLinkClassName={"page"}
+                    breakClassName={"page"}
+                    nextLinkClassName={"page"}
+                    pageClassName={"page"}
+                    disabledClassNae={"disabled"}
+                    activeClassName={"active"}
+                  />
+                </div>
               </div>
             )}
           />
