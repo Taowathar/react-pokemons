81fcac4b4772cdaebb8c4c51b0c313e0af4cb321
Taowathar <taowathar@gmail.com>
2021-08-28 19:41:51 +0200

    Add detail variables to PokemonDetail component

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,16 +1,35 @@
 import { useHttp } from "../hooks/http";
 
 const PokemonDetail = ({ pokemonId }) => {
-  let pokemon = null;
+  let pokemon,
+    name,
+    height,
+    weight,
+    picture,
+    species = null;
+  let abilities = [];
+  let types = [];
 
   const [, fetchData] = useHttp(
     `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`,
     []
   );
+
   if (fetchData) {
     pokemon = fetchData;
+    name = pokemon.name;
+    height = pokemon.height;
+    weight = pokemon.weight;
+    picture = pokemon.sprites.other["official-artwork"]["front_default"];
+    species = pokemon.species.name;
+    abilities = pokemon.abilities;
+    types = pokemon.types;
   }
-  return <div></div>;
+  return (
+    <div className="detail-container">
+
+    </div>
+  );
 };
 
 export default PokemonDetail;
