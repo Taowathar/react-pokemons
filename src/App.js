df1b870fabba14a00d0e21df75722fcb7baecfe2
Taowathar <taowathar@gmail.com>
2021-08-28 13:51:04 +0200

    Replace fetchTypes with http hook in App.js

--- src/App.js
+++ src/App.js
@@ -4,39 +4,35 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import PokemonList from "./components/PokemonList";
 import TypeList from "./components/TypeList";
+import { useHttp } from "./hooks/http";
 
 const App = () => {
   const [pokemonList, setPokemonList] = useState([]);
-  const [typeList, setTypeList] = useState([]);
 
   var [currentPage, setCurrentPage] = useState(0);
   const pageCount = 56;
   const pokemonsPerPage = 20;
+
+  let typeList = null;
+  const [, fetchedTypeList] = useHttp("https://pokeapi.co/api/v2/type", []);
+
+  if (fetchedTypeList) {
+    typeList = fetchedTypeList.results;
+  }
 
   useEffect(() => {
     const getPokemons = async () => {
       const pokemonsFromServer = await fetchPokemons({ currentPage });
       setPokemonList(pokemonsFromServer);
     };
-    const getTypes = async () => {
-      const typesFromServer = await fetchTypes();
-      setTypeList(typesFromServer);
-    };
+
     getPokemons();
-    getTypes();
   }, []);
 
   const fetchPokemons = async ({ currentPage }) => {
     const pokemonsFrom = currentPage * pokemonsPerPage;
     const pokemonListUrl = `https://pokeapi.co/api/v2/pokemon?offset=${pokemonsFrom}&limit=${pokemonsPerPage}`;
     const results = await fetch(pokemonListUrl);
-    const data = await results.json();
-
-    return data.results;
-  };
-
-  const fetchTypes = async () => {
-    const results = await fetch("https://pokeapi.co/api/v2/type");
     const data = await results.json();
 
     return data.results;
